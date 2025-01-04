9d3424c11b6bc26c3e80579948dad49ede1589ac
Sara Fernandes <sarajardimmm@gmail.com>
2025-01-04 22:21:37 +0000

    teste

---
+++ app/src/main/java/com/example/mymadeiraapp/fragments/MapFragment.kt
@@ -0,0 +1,4 @@
+package com.example.mymadeiraapp.fragments
+
+class MapFragment {
+}
---
+++ app/src/main/java/com/example/mymadeiraapp/fragments/MapScreen.kt
@@ -0,0 +1,7 @@
+package com.example.mymadeiraapp.fragments
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MapScreen() {
+}
